9f33aa7e02bec01a3e6351bdab47a48f9d786f32
Marcelo Felix Salgado <marcelo.felixs@gmail.com>
2023-07-19 17:19:45 -0300

    push on main

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := Sum(15, 15)
+	total := Sum(150, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
